77ac8f52d8ca548cc3e281299b057b34a9f51a81
iradaglly <iradagulaliyeva18@mail.ru>
2023-03-02 04:08:10 +0400

    tried to write algorithm

---
+++ 1march(practise task)/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _1march_practise_task_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter number between 10 and 20:");
+
+            string num = Console.ReadLine();
+            int numbers = Convert.ToInt32(num);
+            if (numbers > 10 && numbers < 20)
+            {
+                Console.WriteLine(numbers);
+
+            }
+            else
+            {
+
+                do
+                {
+                    Console.WriteLine("ERROR!Do you want to continue?");
+                    Console.ReadLine();
+                    string yes = Console.ReadLine();
+                    Console.Write("enter new number:");
+                    Console.ReadLine();
+                    string num2 = Console.ReadLine();
+                    int numbers = Convert.ToInt32(num2);
+                    if (numbers % 2 == 0)
+                    {
+                        Console.Write("the number is even");
+
+                    }
+                    else if (numbers % 2 == 1)
+                    {
+                        Console.Write("the number is odd");
+                    }
+
+
+
+
+                } while (!(numbers > 10 && numbers < 20));
+            }
+        }
+    }
+}
